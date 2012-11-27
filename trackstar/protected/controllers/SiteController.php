de3ef335c5ba7cd6150e26d4d54bf619a9109db8
Jeff Winesett <jeff.winesett@controlgroup.com>
2012-11-26 21:46:41 -0600

    commenting out the mail functionality in the contact us action for the live example codebase

--- trackstar/protected/controllers/SiteController.php
+++ trackstar/protected/controllers/SiteController.php
@@ -58,8 +58,11 @@ class SiteController extends Controller
 			$model->attributes=$_POST['ContactForm'];
 			if($model->validate())
 			{
+				/*commented out for live site example*/
+				/*
 				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
 				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
+				*/
 				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
 				$this->refresh();
 			}
