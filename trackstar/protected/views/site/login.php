f9a622188f7eb2e95647ce17ffc30a4eabe7b2b5
Jeff Winesett <jeff.winesett@controlgroup.com>
2012-11-28 09:11:50 -0600

    changing the login hint to suggest contacting the author prior to getting access to the site for those who are not following along in the book

--- trackstar/protected/views/site/login.php
+++ trackstar/protected/views/site/login.php
@@ -31,7 +31,7 @@ $this->breadcrumbs=array(
 		<?php echo $form->passwordField($model,'password'); ?>
 		<?php echo $form->error($model,'password'); ?>
 		<p class="hint">
-			Hint: You may login with <tt>demo/demo</tt> or <tt>admin/admin</tt>.
+			This application is intended to be a reference for those following along with the book. If you would like to request access prior to purchasing the book, please <a href="http://www.seesawlabs.com/yii-book/author-contact"> contact the author</a>.
 		</p>
 	</div>
 
