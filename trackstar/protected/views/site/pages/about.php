31cedd2a9097415098206dadb556a553778d6083
Jeff Winesett <jeff.winesett@controlgroup.com>
2012-11-28 23:04:44 -0600

    changing the about page to a short description of what the trackstar application is

--- trackstar/protected/views/site/pages/about.php
+++ trackstar/protected/views/site/pages/about.php
@@ -6,4 +6,4 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is an example of a "static" page.</p>
+<p><strong>TrackStar</strong> is an online, user-based, project management and task-tracking application that provides the long-format example context for the associated <a href="http://www.seesawlabs.com/yii-book">Yii book</a>. Starting from chapter 3 onward, readers of the <a href="http://www.seesawlabs.com/yii-book">book</a> begin the step-by-step journey of building this application from conception through production readiness.</p>
