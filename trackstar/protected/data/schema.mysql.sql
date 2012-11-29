08d732b181af6754892146bf645369c8f119a13c
Jeff Winesett <jeff.winesett@controlgroup.com>
2012-11-29 17:45:12 -0600

    removing irrelevant schema files and adding in full dump of final schema

--- trackstar/protected/data/schema.mysql.sql
+++ trackstar/protected/data/schema.mysql.sql
@@ -1,28 +1,327 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-);
+-- MySQL dump 10.13  Distrib 5.1.63, for apple-darwin10.8.0 (i386)
+--
+-- Host: localhost    Database: trackstar
+-- ------------------------------------------------------
+-- Server version	5.1.63
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test4', 'pass4', 'test4@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test5', 'pass5', 'test5@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test6', 'pass6', 'test6@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test7', 'pass7', 'test7@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test8', 'pass8', 'test8@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test9', 'pass9', 'test9@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test10', 'pass10', 'test10@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test11', 'pass11', 'test11@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test12', 'pass12', 'test12@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test13', 'pass13', 'test13@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test14', 'pass14', 'test14@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test15', 'pass15', 'test15@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test16', 'pass16', 'test16@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test17', 'pass17', 'test17@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test18', 'pass18', 'test18@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test19', 'pass19', 'test19@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test20', 'pass20', 'test20@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test21', 'pass21', 'test21@example.com');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_auth_assignment`
+--
+
+DROP TABLE IF EXISTS `tbl_auth_assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_auth_assignment` (
+  `itemname` varchar(64) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `bizrule` text,
+  `data` text,
+  PRIMARY KEY (`itemname`,`userid`),
+  KEY `fk_auth_assignment_userid` (`userid`),
+  CONSTRAINT `fk_auth_assignment_userid` FOREIGN KEY (`userid`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_auth_assignment_itemname` FOREIGN KEY (`itemname`) REFERENCES `tbl_auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_auth_assignment`
+--
+
+LOCK TABLES `tbl_auth_assignment` WRITE;
+/*!40000 ALTER TABLE `tbl_auth_assignment` DISABLE KEYS */;
+INSERT INTO `tbl_auth_assignment` VALUES ('admin',1,NULL,'N;');
+/*!40000 ALTER TABLE `tbl_auth_assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_auth_item`
+--
+
+DROP TABLE IF EXISTS `tbl_auth_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_auth_item` (
+  `name` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text,
+  `bizrule` text,
+  `data` text,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_auth_item`
+--
+
+LOCK TABLES `tbl_auth_item` WRITE;
+/*!40000 ALTER TABLE `tbl_auth_item` DISABLE KEYS */;
+INSERT INTO `tbl_auth_item` VALUES ('admin',2,'',NULL,'N;'),('adminManagement',1,'access to the application administration functionality',NULL,'N;'),('createIssue',0,'create a new issue',NULL,'N;'),('createProject',0,'create a new project',NULL,'N;'),('createUser',0,'create a new user',NULL,'N;'),('deleteIssue',0,'delete an issue from a project',NULL,'N;'),('deleteProject',0,'delete a project',NULL,'N;'),('deleteUser',0,'remove a user from a project',NULL,'N;'),('member',2,'',NULL,'N;'),('owner',2,'',NULL,'N;'),('reader',2,'',NULL,'N;'),('readIssue',0,'read issue information',NULL,'N;'),('readProject',0,'read project information',NULL,'N;'),('readUser',0,'read user profile information',NULL,'N;'),('updateIssue',0,'update issue information',NULL,'N;'),('updateProject',0,'update project information',NULL,'N;'),('updateUser',0,'update a users in-formation',NULL,'N;');
+/*!40000 ALTER TABLE `tbl_auth_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_auth_item_child`
+--
+
+DROP TABLE IF EXISTS `tbl_auth_item_child`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_auth_item_child` (
+  `parent` varchar(64) NOT NULL,
+  `child` varchar(64) NOT NULL,
+  PRIMARY KEY (`parent`,`child`),
+  KEY `fk_auth_item_child_child` (`child`),
+  CONSTRAINT `fk_auth_item_child_child` FOREIGN KEY (`child`) REFERENCES `tbl_auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_auth_item_child_parent` FOREIGN KEY (`parent`) REFERENCES `tbl_auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_auth_item_child`
+--
+
+LOCK TABLES `tbl_auth_item_child` WRITE;
+/*!40000 ALTER TABLE `tbl_auth_item_child` DISABLE KEYS */;
+INSERT INTO `tbl_auth_item_child` VALUES ('admin','adminManagement'),('member','createIssue'),('owner','createProject'),('owner','createUser'),('member','deleteIssue'),('owner','deleteProject'),('owner','deleteUser'),('admin','member'),('owner','member'),('admin','owner'),('admin','reader'),('member','reader'),('owner','reader'),('reader','readIssue'),('reader','readProject'),('reader','readUser'),('member','updateIssue'),('owner','updateProject'),('owner','updateUser');
+/*!40000 ALTER TABLE `tbl_auth_item_child` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_comment`
+--
+
+DROP TABLE IF EXISTS `tbl_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `issue_id` int(11) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comment_issue` (`issue_id`),
+  KEY `fk_comment_owner` (`create_user_id`),
+  KEY `fk_comment_update_user` (`update_user_id`),
+  CONSTRAINT `fk_comment_update_user` FOREIGN KEY (`update_user_id`) REFERENCES `tbl_user` (`id`),
+  CONSTRAINT `fk_comment_issue` FOREIGN KEY (`issue_id`) REFERENCES `tbl_issue` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_comment_owner` FOREIGN KEY (`create_user_id`) REFERENCES `tbl_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_comment`
+--
+
+LOCK TABLES `tbl_comment` WRITE;
+/*!40000 ALTER TABLE `tbl_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_issue`
+--
+
+DROP TABLE IF EXISTS `tbl_issue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `project_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `requester_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_issue_project` (`project_id`),
+  KEY `fk_issue_owner` (`owner_id`),
+  KEY `fk_issue_requester` (`requester_id`),
+  CONSTRAINT `fk_issue_requester` FOREIGN KEY (`requester_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_issue_owner` FOREIGN KEY (`owner_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_issue_project` FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_issue`
+--
+
+LOCK TABLES `tbl_issue` WRITE;
+/*!40000 ALTER TABLE `tbl_issue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_issue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_migration`
+--
+
+DROP TABLE IF EXISTS `tbl_migration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_migration` (
+  `version` varchar(255) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_migration`
+--
+
+LOCK TABLES `tbl_migration` WRITE;
+/*!40000 ALTER TABLE `tbl_migration` DISABLE KEYS */;
+INSERT INTO `tbl_migration` VALUES ('m000000_000000_base',1354231742),('m120509_224029_create_project_table',1354231749),('m120511_173401_create_issue_user_and_assignment_tables',1354231750),('m120619_015239_create_rbac_tables',1354231751),('m120620_020255_add_role_to_tbl_project_user_assignment',1354231751),('m120921_015630_create_user_comments_table',1354231752),('m120925_220414_create_system_messages_table',1354231752);
+/*!40000 ALTER TABLE `tbl_migration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_project`
+--
+
+DROP TABLE IF EXISTS `tbl_project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_project`
+--
+
+LOCK TABLES `tbl_project` WRITE;
+/*!40000 ALTER TABLE `tbl_project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_project_user_assignment`
+--
+
+DROP TABLE IF EXISTS `tbl_project_user_assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_project_user_assignment` (
+  `project_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `role` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`project_id`,`user_id`),
+  KEY `fk_user_project` (`user_id`),
+  KEY `fk_project_user_role` (`role`),
+  CONSTRAINT `fk_project_user_role` FOREIGN KEY (`role`) REFERENCES `tbl_auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_project_user` FOREIGN KEY (`project_id`) REFERENCES `tbl_project` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_user_project` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_project_user_assignment`
+--
+
+LOCK TABLES `tbl_project_user_assignment` WRITE;
+/*!40000 ALTER TABLE `tbl_project_user_assignment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_project_user_assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_sys_message`
+--
+
+DROP TABLE IF EXISTS `tbl_sys_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_sys_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sys_message_owner` (`create_user_id`),
+  KEY `fk_sys_message_update_user` (`update_user_id`),
+  KEY `idx_sys_message_update_time` (`update_time`),
+  CONSTRAINT `fk_sys_message_owner` FOREIGN KEY (`create_user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_sys_message_update_user` FOREIGN KEY (`update_user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_sys_message`
+--
+
+LOCK TABLES `tbl_sys_message` WRITE;
+/*!40000 ALTER TABLE `tbl_sys_message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_sys_message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `last_login_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_user_id` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+LOCK TABLES `tbl_user` WRITE;
+/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
+INSERT INTO `tbl_user` VALUES (1,'User One','admin@nosuchaddress.nothing','5a105e8b9d40e1329780d62ea2265d8a','2012-11-29 18:34:26',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-11-29 17:38:45
